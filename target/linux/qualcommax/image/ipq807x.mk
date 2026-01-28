ca706712af9f5cf58ec7bd2071cf7ecd78c79e5d
L1C <43138541+Jim78778@users.noreply.github.com>
2026-01-28 16:22:03 +0800

    Update ipq807x.mk

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -40,6 +40,32 @@ define Build/zyxel-nwa210ax-fit
 	@mv $@.new $@
 endef
 
+define Device/swaiot_cpe_s10
+    $(call Device/FitImage)
+    $(call Device/UbiFit)
+    
+    DEVICE_VENDOR := Swaiot
+    DEVICE_MODEL := CPE-S10
+    DEVICE_VARIANT := NAND
+    SOC := ipq807x
+    DEVICE_DTS := ipq8071-s10
+    
+    # NAND 参数
+    BLOCKSIZE := 128k
+    PAGESIZE := 2048
+    KERNEL_IN_UBI := 1
+    
+    # 直接指定 FIT 构建命令，确保设备树被包含
+    KERNEL = kernel-bin | gzip | fit gz $$(DTS_DIR)/$$(DEVICE_DTS).dtb
+    KERNEL_NAME := zImage
+    KERNEL_LOADADDR := 0x41000000
+    KERNEL_ENTRY_POINT := 0x41000000
+    
+    IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+  
+TARGET_DEVICES += swaiot_cpe_s10
+
 define Device/aliyun_ap8220
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
